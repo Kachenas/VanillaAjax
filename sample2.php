8bcea082ffc90569bb906e87e5cf2650d13f569c
Kachenas <almonte.s.chester@gmail.com>
2023-05-11 15:36:00 +0800

    updated sending of data

--- sample2.php
+++ sample2.php
@@ -1,14 +1,28 @@
 <?php 
-
-include "sample.php";
+//working file
+// include "sample.php";
 
 // $txt = $_POST['txt'];
 // $txt1 = $_POST['txt1'];
 
-if (isset($_POST)) {
-    $data = file_get_contents("php://input");
-    $user = json_decode($data,true);
-    echo json_encode($user);
+// $user = ['txt' => $txt, 'txt1' => $txt1];
+
+// if (isset($_POST)) {
+//     $data = file_get_contents("php://input");
+//     $user = json_decode($data,true);
+//     echo json_encode($user);
     
-}
-?>
+// }
+//echo json_encode($user);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+  
+   
+    $user = ['name' => $name, 'email' => $email, 'message' => $message];
+    echo json_encode($user);
+  }
+  
+  ?>
--- script.js
+++ script.js
@@ -1,24 +1,45 @@
+//working file
+// function registerMe () {
 
-
-
-function registerMe () {
-
-    let user = {
-        "username": "kachenas",
-        "name": "chester",
-        "gender": "male"
-    }
+//     let user = {
+//         "username": "kachenas",
+//         "name": "chester",
+//         "gender": "male"
+//     }
     
-    fetch("sample2.php", {
-        "method": "POST",
-        "headers": {
-            "Content-Type": "application/json;charset=utf-8"
-        },
-        "body": JSON.stringify(user)
-    }).then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        console.log(data);
-    })
+//     fetch("sample2.php", {
+//         "method": "POST",
+//         "headers": {
+//             "Content-Type": "application/json;charset=utf-8"
+//         },
+//         "body": JSON.stringify(user)
+//     }).then(function (response) {
+//         return response.json();
+//     }).then(function (data) {
+//         console.log(data);
+//     })
    
-}
+// }
+
+const form = document.getElementById('registerForm');
+
+form.addEventListener('submit', function(event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Send a POST request to the PHP file with the form data
+  fetch('sample2.php', {
+    method: 'POST',
+    body: new FormData(form)
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Do something with the response from the PHP file
+    //console.log(data);
+    const output = 'Hello ' + data.name + ' with email of ' + data.email + ' your message is ' + data.message;
+    document.getElementById('output').innerHTML = output;
+  })
+  .catch(error => {
+    console.error(error);
+  });
+});
